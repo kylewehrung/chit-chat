eecc605dc5b4023751cab98c4808b84c73a252a6
Kyle Wehrung <kyle.wehrung@gmail.com>
2024-01-19 13:29:07 -0700

    restore requirements.txt

--- server/app.py
+++ server/app.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
+from werkzeug.datastructures import ImmutableDict
+from flask.globals import _request_ctx_stack
 
 app = Flask(__name__)
-api = Api(app)  
+api = Api(app)
 
 class HelloWorld(Resource):
-    def get(self):
-        return 'hello world!'
-    
+   def get(self):
+       return 'hello world!'
+
 api.add_resource(HelloWorld, "/hello_world")
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=False)
+@app.before_request
+def before_request():
+   ctx = _request_ctx_stack.top
+   ctx.aws_context = ImmutableDict(request.environ['aws.context'])
+
+def lambda_handler(event, context):
+   return app(event, context)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/kylewehrung/Development/code/bag-talk/chit-chat/venv"
+set -gx VIRTUAL_ENV "/home/kylewehrung/Development/code/bag-talk/chit-chat/server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
