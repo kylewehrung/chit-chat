d2ebf22e88ecff9e663eed776e311e30340e518f
Kyle Wehrung <kyle.wehrung@gmail.com>
2024-01-20 18:35:21 -0700

    backend deployed and working!

--- server/app.py
+++ server/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, request
 from flask_restful import Api, Resource
-from werkzeug.datastructures import ImmutableDict
-from flask.globals import _request_ctx_stack
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,11 +9,3 @@ class HelloWorld(Resource):
        return 'hello world!'
 
 api.add_resource(HelloWorld, "/hello_world")
-
-@app.before_request
-def before_request():
-   ctx = _request_ctx_stack.top
-   ctx.aws_context = ImmutableDict(request.environ['aws.context'])
-
-def lambda_handler(event, context):
-   return app(event, context)
