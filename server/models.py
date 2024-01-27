69f2c88c1b61efc27d9ce52e0073cb0a3105c365
Kyle Wehrung <kyle.wehrung@gmail.com>
2024-01-27 16:18:00 -0700

    connecting alembic file to postgresql db, still testing

--- server/models.py
+++ server/models.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import db
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class User(db.Model):
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
