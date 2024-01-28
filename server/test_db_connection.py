cbbff580f92f96d0c4f9dc1c6e37644b687b38d9
Kyle Wehrung <kyle.wehrung@gmail.com>
2024-01-27 21:51:23 -0700

    database set up! alembic able to access env variables!!

--- server/test_db_connection.py
+++ server/test_db_connection.py
@@ -10,8 +10,6 @@ try:
     
     # Connect to the database
     with engine.connect() as connection:
-        # Perform a simple query
-        result = connection.execute("SELECT 1")
-        print("Connection successful. Result:", result.fetchone()[0])  # Fetch the result of the query
+        print("Connection successful.")
 except Exception as e:
     print("Error connecting to the database:", e)
