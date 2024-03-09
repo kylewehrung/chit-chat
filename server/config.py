23362efc793d5a3c0780a6ef1de9a8b00c4e09af
Kyle Wehrung <kyle.wehrung@gmail.com>
2024-03-09 16:53:18 -0700

    messed up my config file from last commit

--- server/config.py
+++ server/config.py
@@ -6,31 +6,33 @@ import os
 
 load_dotenv() # Load environment variables from .env file
 
-def create_app():
-    app = Flask(__name__)
-
+class Config:
+    # Secret key for session management and security
+    SECRET_KEY = os.getenv('SECRET_KEY') 
+    
     # Check if the application is running in development mode
     FLASK_ENV = os.getenv('FLASK_ENV', 'development')
-
-    # Function to correct the dialect in the DATABASE_URL
+    
+   # Function to correct the dialect in the DATABASE_URL
     def correct_database_url(url):
         return url.replace('postgres://', 'postgresql://')
 
     # Use the corrected DATABASE_URL for production
-    if FLASK_ENV == 'production':
+    if os.getenv('FLASK_ENV') == 'production':
         SQLALCHEMY_DATABASE_URI = correct_database_url(os.getenv('DATABASE_URL'))
     else:
         SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_URL')
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # Disable Flask-SQLAlchemy event system
+        
+    # Debug mode setting
+    DEBUG = True if FLASK_ENV == 'development' else False
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Disable Flask-SQLAlchemy event system
-    app.config['DEBUG'] = True if FLASK_ENV == 'development' else False
 
-    db = SQLAlchemy(app)
+# Create Flask app and SQLAlchemy instance
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
 
-    # Initialize API
-    api = Api(app)
-
-    return app
-
-app = create_app()
+# Initialize API
+api = Api(app)
