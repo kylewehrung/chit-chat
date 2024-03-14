576bc2b968534209d8ac5dad76a67dc620a3dd51
Kyle Wehrung <kyle.wehrung@gmail.com>
2024-03-14 15:32:01 -0600

    changing cors to only allow requests from amplify url or local host

--- server/app.py
+++ server/app.py
@@ -15,10 +15,10 @@ FLASK_ENV = os.getenv('FLASK_ENV', 'development')
 # Set the base URL for API endpoints based on the environment
 if FLASK_ENV == 'production':
     BASE_URL = '/api'  
-    CORS(app, supports_credentials=True, origins=['*']) # Come back to these, past iteration was still getting blocked by cors
+    CORS(app, supports_credentials=True, origins=['https://main.dgifr50bzat5p.amplifyapp.com/']) 
 else:
     BASE_URL = '/api'  
-    CORS(app, supports_credentials=True, origins=['*'])
+    CORS(app, supports_credentials=True, origins=['http://localhost:4000'])
     print('development')
 
 
